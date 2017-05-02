980ed34a8cd4b4a65d5d17ba6cc51d65d663e837
Chandan Kumar <chandan.jhun@gmail.com>
2017-05-02 21:17:13 +0530

    Final url string response

--- lib/final_redirect_url.rb
+++ lib/final_redirect_url.rb
@@ -1,14 +1,16 @@
 require 'final_redirect_url/version'
+require 'net/http'
 
 module FinalRedirectUrl
 
   def self.final_redirect_url(url, options={})
-    unless is_valid_url?(url)
-      return ''
-    else
+    final_url = ''
+    if is_valid_url?(url)
       redirect_lookup_depth = options[:depth].to_i > 0 ? options[:depth].to_i : 10
-      get_final_redirect_url(url, redirect_lookup_depth)
+      response_uri = get_final_redirect_url(url, redirect_lookup_depth)
+      final_url =  url_string_from_uri(response_uri)
     end
+    final_url
   end
 
   private
@@ -18,17 +20,25 @@ module FinalRedirectUrl
 
   def self.get_final_redirect_url(url, limit = 10)
     uri = URI.parse(url)
-    response = Net::HTTP.get_response(uri)
+    response = ::Net::HTTP.get_response(uri)
     if response.class == Net::HTTPOK
       return uri
     else
       redirect_location = response['location']
       location_uri = URI.parse(redirect_location)
-      if location_uri.host.blank?
+      if location_uri.host.nil?
         redirect_location = uri.scheme + '://' + uri.host + redirect_location
       end
       warn "redirected to #{redirect_location}"
       get_final_redirect_url(redirect_location, limit - 1)
     end
+  end
+
+  def self.url_string_from_uri(uri)
+    url_str = "#{uri.scheme}://#{uri.host}#{uri.request_uri}"
+    if uri.fragment
+      url_str = url_str + "##{uri.fragment}"
+    end
+    url_str
   end
 end
