6aed9b6f88d398d4e909a6e4e052f4e10ebb7286
Chandan Jhunjhunwal <chandan.jhun@coupa.com>
2023-05-30 23:19:10 +0530

    fixed the private method access

--- lib/final_redirect_url.rb
+++ lib/final_redirect_url.rb
@@ -19,33 +19,34 @@ module FinalRedirectUrl
     final_url
   end
 
-  private
-  def self.is_valid_url?(url)
-    url.to_s.match? URI::regexp(['http', 'https'])
-  end
+  class << self
+    def self.is_valid_url?(url)
+      url.to_s.match? URI::regexp(['http', 'https'])
+    end
 
-  def self.get_final_redirect_url(url, limit = 10)
-    return url if limit <= 0
-    uri = URI.parse(url)
-    response = ::Net::HTTP.get_response(uri)
-    if response.class == Net::HTTPOK
-      return uri
-    else
-      redirect_location = response['location']
-      location_uri = URI.parse(redirect_location)
-      if location_uri.host.nil?
-        redirect_location = uri.scheme + '://' + uri.host + redirect_location
+    def self.get_final_redirect_url(url, limit = 10)
+      return url if limit <= 0
+      uri = URI.parse(url)
+      response = ::Net::HTTP.get_response(uri)
+      if response.class == Net::HTTPOK
+        return uri
+      else
+        redirect_location = response['location']
+        location_uri = URI.parse(redirect_location)
+        if location_uri.host.nil?
+          redirect_location = uri.scheme + '://' + uri.host + redirect_location
+        end
+        warn "redirected to #{redirect_location}"
+        get_final_redirect_url(redirect_location, limit - 1)
       end
-      warn "redirected to #{redirect_location}"
-      get_final_redirect_url(redirect_location, limit - 1)
     end
-  end
 
-  def self.url_string_from_uri(uri)
-    url_str = "#{uri.scheme}://#{uri.host}#{uri.request_uri}"
-    if uri.fragment
-      url_str = url_str + "##{uri.fragment}"
+    def self.url_string_from_uri(uri)
+      url_str = "#{uri.scheme}://#{uri.host}#{uri.request_uri}"
+      if uri.fragment
+        url_str = url_str + "##{uri.fragment}"
+      end
+      url_str
     end
-    url_str
   end
 end
